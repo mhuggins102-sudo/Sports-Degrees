27618d96dedbf9e9dae222aeaabe2a7c2b4a00ab
mhuggins102-sudo <mhuggins102@gmail.com>
2026-02-20 15:59:48 -0500

    Add files via upload

---
+++ src/data/mlbData.ts
@@ -0,0 +1,136 @@
+
+import { PlayerHistory } from './nflData';
+
+export const MLB_PLAYERS: Record<string, PlayerHistory> = {
+  "Derek Jeter": { teams: [{ name: "New York Yankees", start: 1995, end: 2014 }] },
+  "Alex Rodriguez": { teams: [{ name: "Seattle Mariners", start: 1994, end: 2000 }, { name: "Texas Rangers", start: 2001, end: 2003 }, { name: "New York Yankees", start: 2004, end: 2016 }] },
+  "Ken Griffey Jr.": { teams: [{ name: "Seattle Mariners", start: 1989, end: 1999 }, { name: "Cincinnati Reds", start: 2000, end: 2008 }, { name: "Chicago White Sox", start: 2008, end: 2008 }, { name: "Seattle Mariners", start: 2009, end: 2010 }] },
+  "Randy Johnson": { teams: [{ name: "Montreal Expos", start: 1988, end: 1989 }, { name: "Seattle Mariners", start: 1989, end: 1998 }, { name: "Houston Astros", start: 1998, end: 1998 }, { name: "Arizona Diamondbacks", start: 1999, end: 2004 }, { name: "New York Yankees", start: 2005, end: 2006 }, { name: "Arizona Diamondbacks", start: 2007, end: 2008 }, { name: "San Francisco Giants", start: 2009, end: 2009 }] },
+  "Ichiro Suzuki": { teams: [{ name: "Seattle Mariners", start: 2001, end: 2012 }, { name: "New York Yankees", start: 2012, end: 2014 }, { name: "Miami Marlins", start: 2015, end: 2017 }, { name: "Seattle Mariners", start: 2018, end: 2019 }] },
+  "Mariano Rivera": { teams: [{ name: "New York Yankees", start: 1995, end: 2013 }] },
+  "Robinson Cano": { teams: [{ name: "New York Yankees", start: 2005, end: 2013 }, { name: "Seattle Mariners", start: 2014, end: 2018 }, { name: "New York Mets", start: 2019, end: 2020 }, { name: "San Diego Padres", start: 2022, end: 2022 }, { name: "Atlanta Braves", start: 2022, end: 2022 }] },
+  "CC Sabathia": { teams: [{ name: "Cleveland Indians", start: 2001, end: 2008 }, { name: "Milwaukee Brewers", start: 2008, end: 2008 }, { name: "New York Yankees", start: 2009, end: 2019 }] },
+  "David Ortiz": { teams: [{ name: "Minnesota Twins", start: 1997, end: 2002 }, { name: "Boston Red Sox", start: 2003, end: 2016 }] },
+  "Manny Ramirez": { teams: [{ name: "Cleveland Indians", start: 1993, end: 2000 }, { name: "Boston Red Sox", start: 2001, end: 2008 }, { name: "Los Angeles Dodgers", start: 2008, end: 2010 }, { name: "Chicago White Sox", start: 2010, end: 2010 }, { name: "Tampa Bay Rays", start: 2011, end: 2011 }] },
+  "Pedro Martinez": { teams: [{ name: "Los Angeles Dodgers", start: 1992, end: 1993 }, { name: "Montreal Expos", start: 1994, end: 1997 }, { name: "Boston Red Sox", start: 1998, end: 2004 }, { name: "New York Mets", start: 2005, end: 2008 }, { name: "Philadelphia Phillies", start: 2009, end: 2009 }] },
+  "Mookie Betts": { teams: [{ name: "Boston Red Sox", start: 2014, end: 2019 }, { name: "Los Angeles Dodgers", start: 2020, end: 2024 }] },
+  "Clayton Kershaw": { teams: [{ name: "Los Angeles Dodgers", start: 2008, end: 2024 }] },
+  "Freddie Freeman": { teams: [{ name: "Atlanta Braves", start: 2010, end: 2021 }, { name: "Los Angeles Dodgers", start: 2022, end: 2024 }] },
+  "Albert Pujols": { teams: [{ name: "St. Louis Cardinals", start: 2001, end: 2011 }, { name: "Los Angeles Angels", start: 2012, end: 2021 }, { name: "Los Angeles Dodgers", start: 2021, end: 2021 }, { name: "St. Louis Cardinals", start: 2022, end: 2022 }] },
+  "Mike Trout": { teams: [{ name: "Los Angeles Angels", start: 2011, end: 2024 }] },
+  "Shohei Ohtani": { teams: [{ name: "Los Angeles Angels", start: 2018, end: 2023 }, { name: "Los Angeles Dodgers", start: 2024, end: 2024 }] },
+  "Justin Verlander": { teams: [{ name: "Detroit Tigers", start: 2005, end: 2017 }, { name: "Houston Astros", start: 2017, end: 2022 }, { name: "New York Mets", start: 2023, end: 2023 }, { name: "Houston Astros", start: 2023, end: 2024 }] },
+  "Max Scherzer": { teams: [{ name: "Arizona Diamondbacks", start: 2008, end: 2009 }, { name: "Detroit Tigers", start: 2010, end: 2014 }, { name: "Washington Nationals", start: 2015, end: 2021 }, { name: "Los Angeles Dodgers", start: 2021, end: 2021 }, { name: "New York Mets", start: 2022, end: 2023 }, { name: "Texas Rangers", start: 2023, end: 2024 }] },
+  "Miguel Cabrera": { teams: [{ name: "Florida Marlins", start: 2003, end: 2007 }, { name: "Detroit Tigers", start: 2008, end: 2023 }] },
+  "Bryce Harper": { teams: [{ name: "Washington Nationals", start: 2012, end: 2018 }, { name: "Philadelphia Phillies", start: 2019, end: 2024 }] },
+  "Juan Soto": { teams: [{ name: "Washington Nationals", start: 2018, end: 2022 }, { name: "San Diego Padres", start: 2022, end: 2023 }, { name: "New York Yankees", start: 2024, end: 2024 }] },
+  "Manny Machado": { teams: [{ name: "Baltimore Orioles", start: 2012, end: 2018 }, { name: "Los Angeles Dodgers", start: 2018, end: 2018 }, { name: "San Diego Padres", start: 2019, end: 2024 }] },
+  "Fernando Tatis Jr.": { teams: [{ name: "San Diego Padres", start: 2019, end: 2024 }] },
+  "Barry Bonds": { teams: [{ name: "Pittsburgh Pirates", start: 1986, end: 1992 }, { name: "San Francisco Giants", start: 1993, end: 2007 }] },
+  "Babe Ruth": { teams: [{ name: "Boston Red Sox", start: 1914, end: 1919 }, { name: "New York Yankees", start: 1920, end: 1934 }, { name: "Boston Braves", start: 1935, end: 1935 }] },
+  "Lou Gehrig": { teams: [{ name: "New York Yankees", start: 1923, end: 1939 }] },
+  "Joe DiMaggio": { teams: [{ name: "New York Yankees", start: 1936, end: 1951 }] },
+  "Mickey Mantle": { teams: [{ name: "New York Yankees", start: 1951, end: 1968 }] },
+  "Yogi Berra": { teams: [{ name: "New York Yankees", start: 1946, end: 1963 }, { name: "New York Mets", start: 1965, end: 1965 }] },
+  "Willie Mays": { teams: [{ name: "New York Giants", start: 1951, end: 1957 }, { name: "San Francisco Giants", start: 1958, end: 1972 }, { name: "New York Mets", start: 1972, end: 1973 }] },
+  "Hank Aaron": { teams: [{ name: "Milwaukee Braves", start: 1954, end: 1965 }, { name: "Atlanta Braves", start: 1966, end: 1974 }, { name: "Milwaukee Brewers", start: 1975, end: 1976 }] },
+  "Nolan Ryan": { teams: [{ name: "New York Mets", start: 1966, end: 1971 }, { name: "California Angels", start: 1972, end: 1979 }, { name: "Houston Astros", start: 1980, end: 1988 }, { name: "Texas Rangers", start: 1989, end: 1993 }] },
+  "Reggie Jackson": { teams: [{ name: "Kansas City Athletics", start: 1967, end: 1967 }, { name: "Oakland Athletics", start: 1968, end: 1975 }, { name: "Baltimore Orioles", start: 1976, end: 1976 }, { name: "New York Yankees", start: 1977, end: 1981 }, { name: "California Angels", start: 1982, end: 1986 }, { name: "Oakland Athletics", start: 1987, end: 1987 }] },
+  "Rickey Henderson": { teams: [{ name: "Oakland Athletics", start: 1979, end: 1984 }, { name: "New York Yankees", start: 1985, end: 1989 }, { name: "Oakland Athletics", start: 1989, end: 1993 }, { name: "Toronto Blue Jays", start: 1993, end: 1993 }, { name: "Oakland Athletics", start: 1994, end: 1995 }, { name: "San Diego Padres", start: 1996, end: 1997 }, { name: "Anaheim Angels", start: 1997, end: 1997 }, { name: "Oakland Athletics", start: 1998, end: 1998 }, { name: "New York Mets", start: 1999, end: 2000 }, { name: "Seattle Mariners", start: 2000, end: 2000 }, { name: "San Diego Padres", start: 2001, end: 2001 }, { name: "Boston Red Sox", start: 2002, end: 2002 }, { name: "Los Angeles Dodgers", start: 2003, end: 2003 }] },
+  "Greg Maddux": { teams: [{ name: "Chicago Cubs", start: 1986, end: 1992 }, { name: "Atlanta Braves", start: 1993, end: 2003 }, { name: "Chicago Cubs", start: 2004, end: 2006 }, { name: "Los Angeles Dodgers", start: 2006, end: 2006 }, { name: "San Diego Padres", start: 2007, end: 2008 }, { name: "Los Angeles Dodgers", start: 2008, end: 2008 }] },
+  "Tom Glavine": { teams: [{ name: "Atlanta Braves", start: 1987, end: 2002 }, { name: "New York Mets", start: 2003, end: 2007 }, { name: "Atlanta Braves", start: 2008, end: 2008 }] },
+  "John Smoltz": { teams: [{ name: "Atlanta Braves", start: 1988, end: 2008 }, { name: "Boston Red Sox", start: 2009, end: 2009 }, { name: "St. Louis Cardinals", start: 2009, end: 2009 }] },
+  "Chipper Jones": { teams: [{ name: "Atlanta Braves", start: 1993, end: 2012 }] },
+  "Cal Ripken Jr.": { teams: [{ name: "Baltimore Orioles", start: 1981, end: 2001 }] },
+  "Tony Gwynn": { teams: [{ name: "San Diego Padres", start: 1982, end: 2001 }] },
+  "Wade Boggs": { teams: [{ name: "Boston Red Sox", start: 1982, end: 1992 }, { name: "New York Yankees", start: 1993, end: 1997 }, { name: "Tampa Bay Devil Rays", start: 1998, end: 1999 }] },
+  "Roger Clemens": { teams: [{ name: "Boston Red Sox", start: 1984, end: 1996 }, { name: "Toronto Blue Jays", start: 1997, end: 1998 }, { name: "New York Yankees", start: 1999, end: 2003 }, { name: "Houston Astros", start: 2004, end: 2006 }, { name: "New York Yankees", start: 2007, end: 2007 }] },
+  "Mark McGwire": { teams: [{ name: "Oakland Athletics", start: 1986, end: 1997 }, { name: "St. Louis Cardinals", start: 1997, end: 2001 }] },
+  "Sammy Sosa": { teams: [{ name: "Texas Rangers", start: 1989, end: 1989 }, { name: "Chicago White Sox", start: 1989, end: 1991 }, { name: "Chicago Cubs", start: 1992, end: 2004 }, { name: "Baltimore Orioles", start: 2005, end: 2005 }, { name: "Texas Rangers", start: 2007, end: 2007 }] },
+  "Frank Thomas": { teams: [{ name: "Chicago White Sox", start: 1990, end: 2005 }, { name: "Oakland Athletics", start: 2006, end: 2006 }, { name: "Toronto Blue Jays", start: 2007, end: 2008 }, { name: "Oakland Athletics", start: 2008, end: 2008 }] },
+  "Jim Thome": { teams: [{ name: "Cleveland Indians", start: 1991, end: 2002 }, { name: "Philadelphia Phillies", start: 2003, end: 2005 }, { name: "Chicago White Sox", start: 2006, end: 2009 }, { name: "Los Angeles Dodgers", start: 2009, end: 2009 }, { name: "Minnesota Twins", start: 2010, end: 2011 }, { name: "Cleveland Indians", start: 2011, end: 2011 }, { name: "Philadelphia Phillies", start: 2012, end: 2012 }, { name: "Baltimore Orioles", start: 2012, end: 2012 }] },
+  "Vladimir Guerrero": { teams: [{ name: "Montreal Expos", start: 1996, end: 2003 }, { name: "Anaheim Angels", start: 2004, end: 2009 }, { name: "Texas Rangers", start: 2010, end: 2010 }, { name: "Baltimore Orioles", start: 2011, end: 2011 }] },
+  "Adrian Beltre": { teams: [{ name: "Los Angeles Dodgers", start: 1998, end: 2004 }, { name: "Seattle Mariners", start: 2005, end: 2009 }, { name: "Boston Red Sox", start: 2010, end: 2010 }, { name: "Texas Rangers", start: 2011, end: 2018 }] },
+  "Miguel Tejada": { teams: [{ name: "Oakland Athletics", start: 1997, end: 2003 }, { name: "Baltimore Orioles", start: 2004, end: 2007 }, { name: "Houston Astros", start: 2008, end: 2009 }, { name: "Baltimore Orioles", start: 2010, end: 2010 }, { name: "San Diego Padres", start: 2010, end: 2010 }, { name: "San Francisco Giants", start: 2011, end: 2011 }, { name: "Kansas City Royals", start: 2013, end: 2013 }] },
+  "Bartolo Colon": { teams: [{ name: "Cleveland Indians", start: 1997, end: 2002 }, { name: "Montreal Expos", start: 2002, end: 2002 }, { name: "Chicago White Sox", start: 2003, end: 2003 }, { name: "Anaheim Angels", start: 2004, end: 2007 }, { name: "Boston Red Sox", start: 2008, end: 2008 }, { name: "Chicago White Sox", start: 2009, end: 2009 }, { name: "New York Yankees", start: 2011, end: 2011 }, { name: "Oakland Athletics", start: 2012, end: 2013 }, { name: "New York Mets", start: 2014, end: 2016 }, { name: "Atlanta Braves", start: 2017, end: 2017 }, { name: "Minnesota Twins", start: 2017, end: 2017 }, { name: "Texas Rangers", start: 2018, end: 2018 }] },
+  "Zack Greinke": { teams: [{ name: "Kansas City Royals", start: 2004, end: 2010 }, { name: "Milwaukee Brewers", start: 2011, end: 2012 }, { name: "Los Angeles Angels", start: 2012, end: 2012 }, { name: "Los Angeles Dodgers", start: 2013, end: 2015 }, { name: "Arizona Diamondbacks", start: 2016, end: 2019 }, { name: "Houston Astros", start: 2019, end: 2021 }, { name: "Kansas City Royals", start: 2022, end: 2023 }] },
+  "Andrew McCutchen": { teams: [{ name: "Pittsburgh Pirates", start: 2009, end: 2017 }, { name: "San Francisco Giants", start: 2018, end: 2018 }, { name: "New York Yankees", start: 2018, end: 2018 }, { name: "Philadelphia Phillies", start: 2019, end: 2021 }, { name: "Milwaukee Brewers", start: 2022, end: 2022 }, { name: "Pittsburgh Pirates", start: 2023, end: 2024 }] },
+  "Giancarlo Stanton": { teams: [{ name: "Florida Marlins", start: 2010, end: 2017 }, { name: "New York Yankees", start: 2018, end: 2024 }] },
+  "Aaron Judge": { teams: [{ name: "New York Yankees", start: 2016, end: 2024 }] },
+  "Gerrit Cole": { teams: [{ name: "Pittsburgh Pirates", start: 2013, end: 2017 }, { name: "Houston Astros", start: 2018, end: 2019 }, { name: "New York Yankees", start: 2020, end: 2024 }] },
+  "Jose Altuve": { teams: [{ name: "Houston Astros", start: 2011, end: 2024 }] },
+  "Carlos Correa": { teams: [{ name: "Houston Astros", start: 2015, end: 2021 }, { name: "Minnesota Twins", start: 2022, end: 2024 }] },
+  "Francisco Lindor": { teams: [{ name: "Cleveland Indians", start: 2015, end: 2020 }, { name: "New York Mets", start: 2021, end: 2024 }] },
+  "Corey Seager": { teams: [{ name: "Los Angeles Dodgers", start: 2015, end: 2021 }, { name: "Texas Rangers", start: 2022, end: 2024 }] },
+  "Trea Turner": { teams: [{ name: "Washington Nationals", start: 2015, end: 2021 }, { name: "Los Angeles Dodgers", start: 2021, end: 2022 }, { name: "Philadelphia Phillies", start: 2023, end: 2024 }] },
+  "Paul Goldschmidt": { teams: [{ name: "Arizona Diamondbacks", start: 2011, end: 2018 }, { name: "St. Louis Cardinals", start: 2019, end: 2024 }] },
+  "Nolan Arenado": { teams: [{ name: "Colorado Rockies", start: 2013, end: 2020 }, { name: "St. Louis Cardinals", start: 2021, end: 2024 }] }
+};
+
+export const MLB_PUZZLES = [
+  // Easy (Same generation, 2-4 degrees)
+  { start: "Derek Jeter", target: "Alex Rodriguez", difficulty: "Easy" },
+  { start: "Ken Griffey Jr.", target: "Randy Johnson", difficulty: "Easy" },
+  { start: "David Ortiz", target: "Manny Ramirez", difficulty: "Easy" },
+  { start: "Albert Pujols", target: "Mike Trout", difficulty: "Easy" },
+  { start: "Max Scherzer", target: "Justin Verlander", difficulty: "Easy" },
+  { start: "Mookie Betts", target: "Clayton Kershaw", difficulty: "Easy" },
+  { start: "Ichiro Suzuki", target: "Derek Jeter", difficulty: "Easy" },
+  { start: "Bryce Harper", target: "Max Scherzer", difficulty: "Easy" },
+  { start: "Pedro Martinez", target: "David Ortiz", difficulty: "Easy" },
+  { start: "Manny Machado", target: "Fernando Tatis Jr.", difficulty: "Easy" },
+  { start: "Shohei Ohtani", target: "Mike Trout", difficulty: "Easy" },
+  { start: "Freddie Freeman", target: "Mookie Betts", difficulty: "Easy" },
+  { start: "Robinson Cano", target: "Derek Jeter", difficulty: "Easy" },
+  { start: "Aaron Judge", target: "Giancarlo Stanton", difficulty: "Easy" },
+  { start: "Jose Altuve", target: "Justin Verlander", difficulty: "Easy" },
+  { start: "Francisco Lindor", target: "Max Scherzer", difficulty: "Easy" },
+  { start: "Corey Seager", target: "Clayton Kershaw", difficulty: "Easy" },
+  { start: "Trea Turner", target: "Bryce Harper", difficulty: "Easy" },
+  { start: "Paul Goldschmidt", target: "Nolan Arenado", difficulty: "Easy" },
+  { start: "Miguel Cabrera", target: "Max Scherzer", difficulty: "Easy" },
+
+  // Medium (Different generations, 2-5 degrees)
+  { start: "Juan Soto", target: "Aaron Judge", difficulty: "Medium" },
+  { start: "Barry Bonds", target: "Randy Johnson", difficulty: "Medium" },
+  { start: "Rickey Henderson", target: "Ichiro Suzuki", difficulty: "Medium" },
+  { start: "Greg Maddux", target: "Clayton Kershaw", difficulty: "Medium" },
+  { start: "Roger Clemens", target: "Justin Verlander", difficulty: "Medium" },
+  { start: "Cal Ripken Jr.", target: "Manny Machado", difficulty: "Medium" },
+  { start: "Tony Gwynn", target: "Fernando Tatis Jr.", difficulty: "Medium" },
+  { start: "Wade Boggs", target: "Mookie Betts", difficulty: "Medium" },
+  { start: "Mark McGwire", target: "Albert Pujols", difficulty: "Medium" },
+  { start: "Sammy Sosa", target: "Kris Bryant", difficulty: "Medium" }, // Need Kris Bryant? Or use existing
+  { start: "Frank Thomas", target: "Jose Abreu", difficulty: "Medium" }, // Need Abreu
+  { start: "Jim Thome", target: "Bryce Harper", difficulty: "Medium" },
+  { start: "Vladimir Guerrero", target: "Mike Trout", difficulty: "Medium" },
+  { start: "Adrian Beltre", target: "Corey Seager", difficulty: "Medium" },
+  { start: "Miguel Tejada", target: "Manny Machado", difficulty: "Medium" },
+  { start: "Bartolo Colon", target: "Shohei Ohtani", difficulty: "Medium" },
+  { start: "Zack Greinke", target: "George Brett", difficulty: "Medium" }, // Need Brett
+  { start: "Andrew McCutchen", target: "Barry Bonds", difficulty: "Medium" },
+  { start: "Giancarlo Stanton", target: "Gary Sheffield", difficulty: "Medium" }, // Need Sheffield
+  { start: "Gerrit Cole", target: "Roger Clemens", difficulty: "Medium" },
+
+  // Hard (No overlap, 3-7 degrees)
+  { start: "Babe Ruth", target: "Aaron Judge", difficulty: "Hard" },
+  { start: "Lou Gehrig", target: "Derek Jeter", difficulty: "Hard" },
+  { start: "Joe DiMaggio", target: "Bernie Williams", difficulty: "Hard" }, // Need Bernie
+  { start: "Mickey Mantle", target: "Robinson Cano", difficulty: "Hard" },
+  { start: "Yogi Berra", target: "Jorge Posada", difficulty: "Hard" }, // Need Posada
+  { start: "Willie Mays", target: "Barry Bonds", difficulty: "Hard" },
+  { start: "Hank Aaron", target: "Ronald Acuna Jr.", difficulty: "Hard" }, // Need Acuna
+  { start: "Nolan Ryan", target: "Jacob deGrom", difficulty: "Hard" }, // Need deGrom
+  { start: "Reggie Jackson", target: "Giancarlo Stanton", difficulty: "Hard" },
+  { start: "Rickey Henderson", target: "Ronald Acuna Jr.", difficulty: "Hard" },
+  { start: "Greg Maddux", target: "Warren Spahn", difficulty: "Hard" }, // Need Spahn
+  { start: "Tom Glavine", target: "Max Fried", difficulty: "Hard" }, // Need Fried
+  { start: "John Smoltz", target: "Spencer Strider", difficulty: "Hard" }, // Need Strider
+  { start: "Chipper Jones", target: "Austin Riley", difficulty: "Hard" }, // Need Riley
+  { start: "Cal Ripken Jr.", target: "Adley Rutschman", difficulty: "Hard" }, // Need Rutschman
+  { start: "Tony Gwynn", target: "Juan Soto", difficulty: "Hard" },
+  { start: "Wade Boggs", target: "Rafael Devers", difficulty: "Hard" }, // Need Devers
+  { start: "Roger Clemens", target: "Gerrit Cole", difficulty: "Hard" },
+  { start: "Mark McGwire", target: "Paul Goldschmidt", difficulty: "Hard" },
+  { start: "Sammy Sosa", target: "Cody Bellinger", difficulty: "Hard" } // Need Bellinger
+];

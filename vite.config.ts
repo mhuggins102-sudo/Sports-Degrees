4e7782ef738be1a7512d65421c054a91823d5b3f
mhuggins102-sudo <mhuggins102@gmail.com>
2026-02-20 18:13:54 -0500

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -16,12 +16,11 @@ export default defineConfig(({ mode }) => {
     },
     plugins: [react()],
     define: {
-      'process.env.API_KEY': JSON.stringify(env.GEMINI_API_KEY || ''),
-      'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY || '')
+      'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY || ''),
     },
     resolve: {
       alias: {
-        '@': path.resolve(__dirname, '.'),
+        '@': path.resolve(__dirname, './'),
       }
     }
   };
